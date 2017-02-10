c01edbf6ea5b9d893f710cdc8ab699f6fa5e9482
Mike Perry <perrymb@gmail.com>
2017-02-09 19:14:34 -0500

    added radio code starting point

--- include/RadioCommunicationManager.h
+++ include/RadioCommunicationManager.h
@@ -10,15 +10,18 @@
 #include <iostream>
 #include <sstream>
 
-enum Power {
-  /*! \brief Power at -18dbm */
-  P_M18DBM = 0,
-  /*! \brief Power at -12dbm */
-  P_M12DBM = 1,
-  /*! \brief Power at -6dbm */
-  P_M6DBM = 2,
-  /*! \brief Power at 0dbm */
-  P_0DBM = 3
+#include "CCRTPPacket.h"
+
+enum Power
+{
+    /*! \brief Power at -18dbm */
+    P_M18DBM = 0,
+    /*! \brief Power at -12dbm */
+    P_M12DBM = 1,
+    /*! \brief Power at -6dbm */
+    P_M6DBM = 2,
+    /*! \brief Power at 0dbm */
+    P_0DBM = 3
 };
 
 class RadioCommunicationManager
@@ -26,12 +29,43 @@ class RadioCommunicationManager
     public:
         RadioCommunicationManager();
         virtual ~RadioCommunicationManager();
+
     protected:
+
     private:
-        void openUsbDongle();
+        bool openUSBDongle();
+        void closeDevice();
+        bool claimInterface(int nInterface);
+
+        CCRTPPacket* readACK();
+        bool ackReceived();
+        bool usbOK();
+
+        void setDataRate(std::string strDataRate);
+        void setChannel(int nChannel);
+        void setARDTime(int nARDTime);
+        void setARDBytes(int nARDBytes);
+        enum Power power();
+        void setPower(enum Power enumPower);
+        void setAddress(char *cAddress);
+        void setContCarrier(bool bContCarrier);
+        void setARC(int nARC);
+
+        CCRTPPacket *waitForPacket();
+        CCRTPPacket *sendPacket(CCRTPPacket *crtpSend, bool bDeleteAfterwards = false);
+        CCRTPPacket *sendAndReceive(CCRTPPacket *crtpSend, bool bDeleteAfterwards);
+        CCRTPPacket *sendAndReceive(CCRTPPacket *crtpSend, int nPort, int nChannel, bool bDeleteAfterwards = true, int nRetries = 10, int nMicrosecondsWait = 100);
+
+        std::list<CCRTPPacket*> popLoggingPackets();
+
+        CCRTPPacket *writeData(void *vdData, int nLength);
+        bool readData(void *vdData, int &nMaxLength);
+        bool writeControl(void *vdData, int nLength, uint8_t u8Request, uint16_t u16Value, uint16_t u16Index);
+
+        bool sendDummyPacket();
+
         std::list<libusb_device*> listDevices(int nVendorID, int nProductID);
         bool startRadio();
-        void closeDevice();
 
         std::string m_strRadioIdentifier;
 
@@ -49,7 +83,7 @@ class RadioCommunicationManager
         int m_bContCarrier;
         float m_fDeviceVersion;
         bool m_bAckReceived;
-        //std::list<CCRTPPacket*> m_lstLoggingPackets;
+        std::list<CCRTPPacket*> m_lstLoggingPackets;
 
 };
 
